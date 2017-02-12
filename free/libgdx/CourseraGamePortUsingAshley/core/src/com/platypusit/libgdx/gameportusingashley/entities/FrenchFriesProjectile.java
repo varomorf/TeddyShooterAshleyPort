dc29ecfdd5e41f746515596618b7bc9a4206926f
Álvaro Fernández González <varomorf@gmail.com>
2017-02-12 03:07:51 +0100

    Add french fries projectile entity.

---
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/entities/FrenchFriesProjectile.java
@@ -0,0 +1,22 @@
+package com.platypusit.libgdx.gameportusingashley.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import static com.platypusit.libgdx.gameportusingashley.constant.GameConstants.FRENCH_FRIES_PROJECTILE_SPEED;
+
+/**
+ * <p>Entity for projectile shoot off teddy bears.</p>
+ * Created by Alvaro on 12/02/2017.
+ */
+public class FrenchFriesProjectile extends Projectile {
+
+    private static Texture projectileTexture;
+
+    public static void setProjectileTexture(Texture texture){
+        projectileTexture = texture;
+    }
+
+    public FrenchFriesProjectile(float x, float y) {
+        super(x, y, FRENCH_FRIES_PROJECTILE_SPEED, projectileTexture);
+    }
+}
