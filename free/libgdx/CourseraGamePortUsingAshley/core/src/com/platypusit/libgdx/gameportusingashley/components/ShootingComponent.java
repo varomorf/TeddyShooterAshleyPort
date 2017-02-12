9041b10e4093ebaf6f9ef17074eb13d8321487eb
Álvaro Fernández González <varomorf@gmail.com>
2017-02-12 01:35:21 +0100

    Add a shooting component.

---
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/components/ShootingComponent.java
@@ -0,0 +1,16 @@
+package com.platypusit.libgdx.gameportusingashley.components;
+
+import com.badlogic.gdx.audio.Sound;
+import com.platypusit.libgdx.gameportusingashley.constant.ProjectileType;
+
+/**
+ * <p>Component to allow and entity to shoot projectiles.</p>
+ * Created by Alvaro on 12/02/2017.
+ */
+public class ShootingComponent {
+
+    public ProjectileType projectileType;
+    public float projectileSpeed;
+    public Sound firingSound;
+
+}
