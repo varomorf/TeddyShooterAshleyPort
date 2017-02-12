9ad35bac2606c3941394e14ea1fa7ba37fee7c69
Álvaro Fernández González <varomorf@gmail.com>
2017-02-12 01:58:29 +0100

    Add new component for entities that shoot after a number of seconds have passed.

---
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/components/TimedShootingComponent.java
@@ -0,0 +1,13 @@
+package com.platypusit.libgdx.gameportusingashley.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * <p>Component for entities that shoot after a number of seconds have passed.</p>
+ * Created by Alvaro on 12/02/2017.
+ */
+public class TimedShootingComponent implements Component {
+
+    public float timer;
+    public float secondsForShooting;
+}
