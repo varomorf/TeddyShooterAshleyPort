7d2896c2a1cfddab934dc912e44a5ff45b74f2e2
Álvaro Fernández González <varomorf@gmail.com>
2017-02-12 02:42:13 +0100

    Add component for flaggin the player controlled entity.

---
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/components/PlayerComponent.java
@@ -0,0 +1,10 @@
+package com.platypusit.libgdx.gameportusingashley.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * <p>Component for flagging the player controlled entity.</p>
+ * Created by Alvaro on 12/02/2017.
+ */
+public class PlayerComponent implements Component {
+}
