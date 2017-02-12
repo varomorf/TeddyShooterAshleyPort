89e8ca0850671ec8da12e609a0df0664536dd4dc
Álvaro Fernández González <varomorf@gmail.com>
2017-02-12 01:43:37 +0100

    Formatting.

--- free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/ComponentMappers.java
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/ComponentMappers.java
@@ -8,9 +8,9 @@ import com.platypusit.libgdx.gameportusingashley.components.*;
  * Created by alfergon on 30/01/17.
  */
 public class ComponentMappers {
-		public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
-		public static final ComponentMapper<VelocityComponent> velocity = ComponentMapper.getFor(VelocityComponent.class);
-		public static final ComponentMapper<DrawableComponent> drawable = ComponentMapper.getFor(DrawableComponent.class);
-		public static final ComponentMapper<BoundsBounceableComponent> boundsBounceable = ComponentMapper.getFor(BoundsBounceableComponent.class);
+    public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
+    public static final ComponentMapper<VelocityComponent> velocity = ComponentMapper.getFor(VelocityComponent.class);
+    public static final ComponentMapper<DrawableComponent> drawable = ComponentMapper.getFor(DrawableComponent.class);
+    public static final ComponentMapper<BoundsBounceableComponent> boundsBounceable = ComponentMapper.getFor(BoundsBounceableComponent.class);
     public static final ComponentMapper<ShootingComponent> shooting = ComponentMapper.getFor(ShootingComponent.class);
 }
