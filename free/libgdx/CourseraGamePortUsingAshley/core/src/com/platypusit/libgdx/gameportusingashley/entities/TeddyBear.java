ea4de744e26442d0929710116eeb81c4d4cfcc95
Álvaro Fernández González <varomorf@gmail.com>
2017-02-12 02:21:53 +0100

    Set a random initial shooting timer to avoid initial 5 bears shooting at the same time.

--- free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/entities/TeddyBear.java
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/entities/TeddyBear.java
@@ -4,10 +4,11 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.platypusit.libgdx.gameportusingashley.RandomNumberGenerator;
 import com.platypusit.libgdx.gameportusingashley.components.*;
-import com.platypusit.libgdx.gameportusingashley.constant.GameConstants;
 
 import static com.platypusit.libgdx.gameportusingashley.components.factory.BoundsBounceableComponentFactory.worldBounceableComponent;
+import static com.platypusit.libgdx.gameportusingashley.constant.GameConstants.BEAR_FIRING_RATE_RANGE;
 import static com.platypusit.libgdx.gameportusingashley.constant.GameConstants.TEDDY_BEAR_PROJECTILE_OFFSET;
 import static com.platypusit.libgdx.gameportusingashley.constant.ProjectileType.TEDDY_BEAR;
 
@@ -23,6 +24,10 @@ public class TeddyBear extends Entity {
         add(new DrawableComponent(texture));
         add(worldBounceableComponent);
         add(new ShootingComponent(TEDDY_BEAR, -TEDDY_BEAR_PROJECTILE_OFFSET, teddyShot));
-        add(new TimedShootingComponent(GameConstants.BEAR_FIRING_RATE_RANGE));
+        add(new TimedShootingComponent(getInitialTimer(), BEAR_FIRING_RATE_RANGE));
+    }
+
+    protected float getInitialTimer() {
+        return RandomNumberGenerator.nextFloat(-0.25f, 0.25f);
     }
 }
