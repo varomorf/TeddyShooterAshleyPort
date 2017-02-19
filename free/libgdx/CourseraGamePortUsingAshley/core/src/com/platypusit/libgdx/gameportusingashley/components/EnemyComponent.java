ac6f981235d301e02b521539c44e547a084a99da
Álvaro Fernández González <varomorf@gmail.com>
2017-02-19 12:42:39 +0100

    Add flag component for enemy entities.

---
+++ free/libgdx/CourseraGamePortUsingAshley/core/src/com/platypusit/libgdx/gameportusingashley/components/EnemyComponent.java
@@ -0,0 +1,10 @@
+package com.platypusit.libgdx.gameportusingashley.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * <p>Flag component for player enemies.</p>
+ * Created by Alvaro on 19/02/2017.
+ */
+public class EnemyComponent implements Component {
+}
